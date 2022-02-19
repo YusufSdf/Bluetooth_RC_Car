45aa42183ae888ed813df2e8a07c660ac287105b
Yusuf <yusufsedef0@gmail.com>
2022-02-20 00:04:04 +0300

    Ön kısım ayarlandı.

--- Bluetooth_RC_Car/Bluetooth_RC_Car.ino
+++ Bluetooth_RC_Car/Bluetooth_RC_Car.ino
@@ -40,7 +40,7 @@ if (bt_contact.available()) {
 
   if (data == 'S') // Duruyor , Stop
   {
-     delay(500);
+     delay(200);
      analogWrite(e5,0);
      analogWrite(e6,0);
      digitalWrite(in1,LOW);
@@ -52,46 +52,47 @@ if (bt_contact.available()) {
 
   else if (data == 'F') // İleri , forward
   {
-     delay(200);
+     delay(100);
      analogWrite(e5,255);
      analogWrite(e6,255);
-     digitalWrite(in1,LOW);
-     digitalWrite(in2,HIGH);
-     digitalWrite(in3,HIGH);
-     digitalWrite(in4,LOW);  
+     digitalWrite(in1,HIGH);
+     digitalWrite(in2,LOW);
+     digitalWrite(in3,LOW);
+     digitalWrite(in4,HIGH);
+ 
   }
 
   else if (data == 'B')  // Geri , Back
   {
-     delay(200);
+     delay(100);
      analogWrite(e5,155);
      analogWrite(e6,155);
-     digitalWrite(in1,HIGH);
-     digitalWrite(in2,LOW);
-     digitalWrite(in3,LOW);
-     digitalWrite(in4,HIGH);  
-  }
-
-  else if (data == 'R') // Sağ , Right
-  {
-     delay(200);
-     analogWrite(e5,50);
-     analogWrite(e6,100);
      digitalWrite(in1,LOW);
      digitalWrite(in2,HIGH);
      digitalWrite(in3,HIGH);
      digitalWrite(in4,LOW);  
   }
 
+  else if (data == 'R') // Sağ , Right
+  {
+     delay(100);
+     analogWrite(e5,0);
+     analogWrite(e6,255);
+     digitalWrite(in1,HIGH);
+     digitalWrite(in2,LOW);
+     digitalWrite(in3,LOW);
+     digitalWrite(in4,HIGH); 
+  }
+
   else if (data == 'L')  // Sol , Left
   {
-     delay(200);
-     analogWrite(e5,100);
-     analogWrite(e6,50);
-     digitalWrite(in1,LOW);
-     digitalWrite(in2,HIGH);
-     digitalWrite(in3,HIGH);
-     digitalWrite(in4,LOW); 
+     delay(100);
+     analogWrite(e5,255);
+     analogWrite(e6,0);
+     digitalWrite(in1,HIGH);
+     digitalWrite(in2,LOW);
+     digitalWrite(in3,LOW);
+     digitalWrite(in4,HIGH);
      
   }
 
